3a92661ce89aae0ed68e4570727679f8654f9059
Dan Bikle <dan.bikle@gmail.com>
2015-11-29 21:22:53 +0000

    hello

---
+++ wget_cardata.bash
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# ~/app30/wget_cardata.bash
+
+cd ~/Downloads/
+
+/usr/bin/curl http://mldata.org/repository/data/download/csv/datasets-uci-iris/          > iris.data.txt
+/usr/bin/curl https://archive.ics.uci.edu/ml/machine-learning-databases/car/car.data     > car.data.txt
+/usr/bin/curl https://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.data > yeast.data.txt
+
+exit
+
+https://archive.ics.uci.edu/ml/machine-learning-databases/yeast/yeast.data
