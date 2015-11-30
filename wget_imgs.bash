744372b2b84b701aa890fd9ab7d82bc88cbbbe86
Dan Bikle <dan.bikle@gmail.com>
2015-11-30 02:48:58 +0000

    more

---
+++ wget_imgs.bash
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# ~/app30/wget_imgs.bash
+
+rm -rf   ~/Downloads/ak/imgs
+mkdir -p ~/Downloads/ak/imgs
+cd       ~/Downloads/ak/imgs
+
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/cat.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/esher.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/pencils.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/usa.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/fractal.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/dora.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/rubiks.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/earth.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/fox.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/twitter.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/reddit.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/chip.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/jitendra.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/tesla.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/rainforest.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/chess.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/gradient.png
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/battery.jpg
+wget http://cs.stanford.edu/people/karpathy/convnetjs/demo/imgs/starry.jpg
+exit
--- wget_jqui.bash
+++ wget_jqui.bash
@@ -5,7 +5,12 @@
 # I should use exact same jquery bits which AK uses.
 cd ~/Downloads/
 rm -f jquery-ui.min.js jquery-ui.min.css
+rm -f ui-bg_glass_75_e6e6e6_1x400.png
+rm -f ui-bg_flat_75_ffffff_40x100.png
 wget  http://cs.stanford.edu/people/karpathy/convnetjs/demo/jquery-ui.min.js
 wget  http://cs.stanford.edu/people/karpathy/convnetjs/demo/jquery-ui.min.css
+wget https://jquery-ui.googlecode.com/svn/tags/1.8.2/themes/smoothness/images/ui-bg_glass_75_e6e6e6_1x400.png
+wget https://jquery-ui.googlecode.com/svn/tags/1.8.2/themes/smoothness/images/ui-bg_flat_75_ffffff_40x100.png
+
 exit
 
